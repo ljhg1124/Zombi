04a2a8ad300a46fa3211b4ddc217593f22bbd341
LEE JUN HEE <ljhg1124@gmail.com>
2020-08-01 02:47:28 +0900

    적 생성기

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,7 +2,8 @@
 using UnityEngine;
 
 // 적 게임 오브젝트를 주기적으로 생성
-public class EnemySpawner : MonoBehaviour {
+public class EnemySpawner : MonoBehaviour
+{
     public Enemy enemyPrefab; // 생성할 적 AI
 
     public Transform[] spawnPoints; // 적 AI를 소환할 위치들
@@ -21,7 +22,8 @@ public class EnemySpawner : MonoBehaviour {
     private List<Enemy> enemies = new List<Enemy>(); // 생성된 적들을 담는 리스트
     private int wave; // 현재 웨이브
 
-    private void Update() {
+    private void Update()
+    {
         // 게임 오버 상태일때는 생성하지 않음
         if (GameManager.instance != null && GameManager.instance.isGameover)
         {
@@ -39,16 +41,48 @@ public class EnemySpawner : MonoBehaviour {
     }
 
     // 웨이브 정보를 UI로 표시
-    private void UpdateUI() {
+    private void UpdateUI()
+    {
         // 현재 웨이브와 남은 적의 수 표시
         UIManager.instance.UpdateWaveText(wave, enemies.Count);
     }
 
     // 현재 웨이브에 맞춰 적을 생성
-    private void SpawnWave() {
+    private void SpawnWave()
+    {
+        wave++;
+
+        // RoundToInt 반올림
+        int spawnCount = Mathf.RoundToInt(wave * 1.5f);
+
+        for (int i = 0; i < spawnCount; i++)
+        {
+            float enemyIntensity = Random.Range(0f, 1f);
+
+            CreateEnemy(enemyIntensity);
+        }
     }
 
     // 적을 생성하고 생성한 적에게 추적할 대상을 할당
-    private void CreateEnemy(float intensity) {
+    private void CreateEnemy(float intensity)
+    {
+        float health = Mathf.Lerp(healthMin, healthMax, intensity);
+        float damage = Mathf.Lerp(damageMin, damageMax, intensity);
+        float speed = Mathf.Lerp(speedMin, speedMax, intensity);
+
+        Color skinColor = Color.Lerp(Color.white, strongEnemyColor, intensity);
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        Enemy enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        enemy.Setup(health, damage, speed, skinColor);
+
+        enemies.Add(enemy);
+
+        enemy.onDeath += () => enemies.Remove(enemy);
+        enemy.onDeath += () => Destroy(enemy.gameObject, 10f);
+        enemy.onDeath += () => GameManager.instance.AddScore(100);
+
+
     }
 }
